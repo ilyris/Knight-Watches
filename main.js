b5f9d6933e965b4e146614d7e9b7693d42321691
Dylan Sieren <dylan.sieren01@gmail.com>
2018-12-14 07:46:18 -0600

    started to make the navigation a full page slide out navigation, currently have it where the button can be clicked and the navigation menu comes all the way out, but there is currently no way to close it

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const innerButtons = document.getElementsByClassName('inner-button');
-const sliderButtonContainer = document.getElementById('slider-buttons');
+
+    const innerButtons = document.getElementsByClassName('inner-button');
+    const sliderButtonContainer = document.getElementById('slider-buttons');
+    const fullScreenNavigation = document.querySelector(".navigation-slide-container");
+
+    
+    document.querySelector(".hamburger-menu").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  this.classList.toggle("toggle-active");
+  fullScreenNavigation.classList.toggle("on");
+
+}
 
 
 
 
 const toggleSliderButtons = () => {
+
     for (let i = 0; i < innerButtons.length; i++) {
         innerButtons[i].addEventListener('click', function () {
             console.log('clicked');
@@ -15,28 +27,8 @@ const toggleSliderButtons = () => {
         });
     }
 }
+
+
 for(let value of innerButtons) {
     value.addEventListener("click", toggleSliderButtons);
 }
-
-
-// if a inner button has the class active, another button shouldn't have the active class. allowing only one button to be clicked at a time.
-// when innerButton[1] toggles active "on" then innerButton [0] or [2] toggle active off
-
-// for (let i = 0; i < innerButtons.length; i++) {
-//     innerButtons[i].addEventListener("click", () => {
-//         console.log(innerButtons[i]);
-//         // innerButtons[i].classList.toggle('active');
-//         if(innerButtons[i].className = 'active' == false) {
-//                 innerButtons[i].className = 'inner-button active';
-//         }else if (innerButtons[i].classList.contains("active")) {
-//             innerButtons[i].className = "inner-button";
-//         }
-//     })
-// }
-// if Element.classList.contains(active);
-
-
-// const toggleSliderButtonActive = () => {
-
-// }
