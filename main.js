c45e7fbd14d4c728b6d067cdd439cb14650d87f2
Dylan Sieren <dylan.sieren01@gmail.com>
2018-12-15 00:25:22 -0600

    animations now fade in on menu click, then slide out when the menu is closed. Next step is fine tuning the animations, they are currently moving too fast

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
+const innerButtons = document.getElementsByClassName("inner-button");
+const sliderButtonContainer = document.getElementById("slider-buttons");
+const fullScreenNavigation = document.querySelector(".navigation-slide-container");
+const menuLinksLi = document.getElementsByClassName("navigation-li");
 
-    const innerButtons = document.getElementsByClassName('inner-button');
-    const sliderButtonContainer = document.getElementById('slider-buttons');
-    const fullScreenNavigation = document.querySelector(".navigation-slide-container");
-    const menuLinksLi = document.getElementsByClassName('navigation-li');
-
-    
-    document.querySelector(".hamburger-menu").addEventListener("click", toggleMenu);
+document.querySelector(".hamburger-menu").addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   this.classList.toggle("toggle-active");
   fullScreenNavigation.classList.toggle("on");
-    for(value of menuLinksLi) {
-            console.log(value);
-            value.classList.add('fadeInRightBig');
-    }
+  if(fullScreenNavigation.classList.contains('on') == true) {
+    for ( let i = 0; i < menuLinksLi.length; i++) {
+        menuLinksLi[i].classList.add("fadeInRightBig");
+        menuLinksLi[i].classList.remove('fadeOutRightBig');
 
+      }
+  }
+  if (fullScreenNavigation.classList.contains("on") == false) {
+    for (let i = 0; i < menuLinksLi.length; i++) {
+        menuLinksLi[i].classList.add("fadeOutRightBig");
+        menuLinksLi[i].classList.remove('fadeInRightBig');
+      }
+  }
 }
-
-
-
 
 const toggleSliderButtons = () => {
+  for (let i = 0; i < innerButtons.length; i++) {
+    innerButtons[i].addEventListener("click", function() {
+      const activesliderButton = document.getElementsByClassName("active");
+      activesliderButton[0].className = activesliderButton[0].className.replace("active","");
+      this.className += " active";
+    });
+  }
+};
 
-    for (let i = 0; i < innerButtons.length; i++) {
-        innerButtons[i].addEventListener('click', function () {
-            console.log('clicked');
-            const activesliderButton = document.getElementsByClassName('active');
-            activesliderButton[0].className = activesliderButton[0].className.replace('active', '');
-            this.className += ' active';
-            console.log(this);
-        });
-    }
-}
-
-
-for(let value of innerButtons) {
-    value.addEventListener("click", toggleSliderButtons);
+for (let value of innerButtons) {
+  value.addEventListener("click", toggleSliderButtons);
 }
